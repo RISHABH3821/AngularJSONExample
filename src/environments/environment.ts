dfbf4a0926c89efd712288ba3aea1010d1087e07
rshrivastav <rshrivastav@teksystems.com>
2022-01-23 15:37:09 +0530

    deployment

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:3000/'
+  apiUrl: 'https://my-json-server.typicode.com/RISHABH3821/AngularJSONExampleServer/'
 };
 
 /*
